5673dadd4237fe3f49cea25459e75ed6d2690d5d
tommy3wood <thomaswcurran1014@gmail.com>
2020-03-29 13:48:29 -0500

    api setup for vue_express

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -4,14 +4,37 @@ const mongodb = require('mongodb');
 const router = express.Router();
 
 //GET POSTS
-router.get('/', (req, res) => {
-  res.send('hello');
+router.get('/', async (req, res) => {
+  const posts = await loadPostCollection();
+  res.send(await posts.find({}).toArray());
 });
 
 //ADD POSTS
 
+router.post('/', async (req, res) => {
+  const posts = await loadPostCollection();
+  await posts.insertOne({
+    text: req.body.text,
+    createdAt: new Date()
+  });
+  res.status(201).send();
+});
 
 //DELETE POSTS
+router.delete('/:id', async (req, res) => {
+  const posts = await loadPostCollection();
+  await posts.deleteOne({_id: new mongodb.ObjectID(req.params.id)});
+  res.status(200).send();
+});
 
+
+async function loadPostCollection() {
+  const client = await mongodb.MongoClient.connect
+  ('mongodb+srv://tome3wood:Forum1991!@cluster0-adnf5.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+  });
+
+  return client.db('Cluster0').collection('posts')
+}
 
 module.exports = router;
